f54590bb0adb6314cd47dd4e8c8e018a8fe139aa
Jerrick-Santos-DLSU <jerrickezekiel0122@gmail.com>
2024-11-10 23:15:41 +0800

    test route

--- routes/commonRoute.js
+++ routes/commonRoute.js
@@ -16,6 +16,21 @@ module.exports = (db) => {
             }
         });
     });
+
+
+    router.get('/test', (req, res) => {
+        db.query(`SELECT * FROM mobdeve_schema.MD_barangays;`, (err, results) => {
+            if (err) {
+                console.error("Database query error:", err);
+                res.status(500).json({ error: "Internal Server Error" });
+            } else if (results.length === 0) {
+                console.log("No barangays found.");
+                res.status(404).json({ message: "No barangays found" });
+            } else {
+                res.status(200).json({ message: "Barangays fetched successfully", data: results });
+            }
+        });
+    });
     
 
     return router;
