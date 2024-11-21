7a4cfbd2c05c128c50ac568d5292b886e853347b
peterpanda00 <mjperez0910@gmail.com>
2024-11-21 17:06:48 +0800

    mali ni migs update

--- routes/patientViewRoute.js
+++ routes/patientViewRoute.js
@@ -31,8 +31,8 @@ module.exports = (db) => {
 
     router.get('/patientprescriptionitems/:id', (req, res) => {
         const patientID = req.params.id; // Access the patientID from the URL parameter
-        const dbquery = `SELECT 	DISTINCT pc.product_name,
-                                    pi.presc_item_id AS product_ID
+        const dbquery = `SELECT 	DISTINCT pc.product_ID,
+                                    pc.product_name
                             FROM 	mobdeve_schema.TD_prescription p
                             JOIN 	mobdeve_schema.TD_prescription_items pi 	ON pi.presc_id = p.presc_id
                             JOIN 	mobdeve_schema.MD_patient	pt				ON pt.patient_id = p.patient_id
@@ -196,6 +196,30 @@ module.exports = (db) => {
 
     router.get('/prescriptions/:id', (req, res) => {
         const patientID = req.params.id; // Access the patientID from the URL parameter
+        const dbquery = `SELECT 	p.presc_ID AS presc_ID , 
+                                    u.last_name AS doctor_last_name, 
+                                    u.first_name AS doctor_first_name, 
+                                    p.date_created AS prescription_date_created
+                            FROM	TD_prescription p
+                            JOIN	MD_doctor d ON p.doctor_ID = d.doctor_ID
+                            JOIN	MD_user u ON d.user_ID = u.user_ID
+                            WHERE 	patient_ID = ?`;
+
+        db.query(dbquery, [patientID], (err, results) => {
+            if (err) {
+                console.error("Database query error:", err);
+                res.status(500).json({ error: "Internal Server Error" });
+            } else if (results.length === 0) {
+                console.log("Prescriptions Not Found.");
+                res.status(404).json({ message: "Prescriptions Not Found" });
+            } else {
+                res.status(200).json(results);
+            }
+        })
+    })
+
+    router.get('/medications/:id', (req, res) => {
+        const prescID = req.params.id; // Access the patientID from the URL parameter
         const dbquery = `SELECT	    pi.presc_item_ID AS presc_item_ID,
                                     pcat.product_name AS product_name,
                                     b.name AS brand_name,
@@ -206,10 +230,7 @@ module.exports = (db) => {
                                     pi.amt_needed AS amt_needed,
                                     pi.take_morning AS take_morning,
                                     pi.take_noon AS take_noon,
-                                    pi.take_night AS take_night,
-                                    usr.first_name AS doctor_first_name,
-                                    usr.last_name AS doctor_last_name,
-                                    p.date_created AS prescription_date_created
+                                    pi.take_night AS take_night
                             FROM	TD_prescription_items pi
                             JOIN	CMD_product_catalogue pcat ON pi.product_ID = pcat.product_ID
                             JOIN 	CMD_brand b ON pcat.brand_ID = b.brand_ID
@@ -219,9 +240,9 @@ module.exports = (db) => {
                             JOIN	TD_prescription p ON pi.presc_ID = p.presc_ID
                             JOIN	MD_doctor d ON p.doctor_ID = d.doctor_ID
                             JOIN	MD_user usr ON d.user_ID = usr.user_ID
-                            WHERE	p.patient_ID = ?`;
+                            WHERE	pi.presc_ID = ?`;
 
-        db.query(dbquery, [patientID], (err, results) => {
+        db.query(dbquery, [prescID], (err, results) => {
             if (err) {
                 console.error("Database query error:", err);
                 res.status(500).json({ error: "Internal Server Error" });
